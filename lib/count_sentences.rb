2302e1f04ef6cd561afdd506ebff7a070affca70
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-09 05:34:28 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
+    self.ends_with?(".")
     
   end
 
   def question?
-
+    self.ends_with("?")
   end
 
   def exclamation?
-
+    self.ends_with("!")
   end
 
   def count_sentences
-
+    total = self.count
   end
 end
